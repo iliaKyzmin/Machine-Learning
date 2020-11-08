ba7ce2eda3abf0ef4511ce3aa47455811d13da67
Ilia Kuzmin <kyzmin.ilia@yandex.ru>
2020-11-08 21:44:39 +0300

    added data plot for 3rd task of knn lab

--- k_nearest_neighbors/task_3.R
+++ k_nearest_neighbors/task_3.R
@@ -3,6 +3,12 @@ library(kknn)
 train_data <- read.table('data/svmdata4.txt', sep = '\t', header = T, stringsAsFactors = T)
 test_data <- read.table('data/svmdata4test.txt', sep = '\t', header = T, stringsAsFactors = T)
 
+
+plot(train_data$X1, 
+     train_data$X2, pch=21, 
+     bg=c("green","red") [unclass(train_data$Colors)],  
+     main="Train data")
+
 clsfr <- train.kknn(Colors ~ ., 
                     data = train_data, 
                     kmax = 15, 
@@ -13,3 +19,4 @@ print(clsfr[["best.parameters"]])
 predicted <- predict(clsfr, test_data)
 tbl <- table(predicted, test_data$Colors)
 print(paste("Accuracy - ", as.character((tbl[1, 1] + tbl[2, 2]) / nrow(test_data))))
+
