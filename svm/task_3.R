7e81e9556afbf08eb893a622c5ec8eca16185085
Ilia Kuzmin <kyzmin.ilia@yandex.ru>
2020-11-24 21:37:42 +0300

    added third task

--- svm/task_3.R
+++ svm/task_3.R
@@ -7,7 +7,7 @@ find_optimal_kernel <- function(kernel) {
   svm_clsfr <- svm(Colors ~ ., 
                    data = train_data,
                    type = "C-classification",
-                   cost = 2,
+                   cost = 1,
                    kernel = kernel)
   
     pred <- predict(svm_clsfr, test_data)
@@ -18,3 +18,21 @@ find_optimal_kernel <- function(kernel) {
 print(paste("Polynomial - ", find_optimal_kernel("polynomial")))
 print(paste("Radial - ", find_optimal_kernel("radial")))
 print(paste("Sigmoid - ", find_optimal_kernel("sigmoid")))
+
+
+get_pol_error <- function(degree) {
+  svm_clsfr <- svm(Colors ~ ., 
+                   data = train_data,
+                   type = "C-classification",
+                   cost = 1,
+                   kernel = "polynomial",
+                   degree = degree)
+  
+  pred <- predict(svm_clsfr, test_data)
+  tbl <- table(pred, test_data$Color)
+  return(1. - sum(diag(tbl)) / nrow(test_data))
+}
+
+for (deg in 1:20) {
+  print(paste('Degree ', deg, ' - ', get_pol_error(deg)))
+}
